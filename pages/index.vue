fa7f95e69123bf987f7630126513442ee8833cc1
Allen Hurff <allenhurff@gmail.com>
2025-03-06 08:29:05 -0500

    Fixed home page SVG blocking the run-of-show.

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
             <p class="text-xl mb-8">
               Discover premium vending machine solutions that elevate your business and delight your customers.
             </p>
-            <div class="flex flex-wrap gap-4">
+            <div class="flex flex-wrap gap-4 relative z-10">
               <NuxtLink to="/products" class="btn-primary">
                 Explore Products
               </NuxtLink>
@@ -32,7 +32,7 @@
       </div>
       
       <!-- Wave Divider -->
-      <div class="absolute bottom-0 left-0 right-0">
+      <div class="absolute bottom-0 left-0 right-0 pointer-events-none z-0">
         <svg viewBox="0 0 1440 100" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
           <path d="M0 0L48 8.33333C96 16.6667 192 33.3333 288 41.6667C384 50 480 50 576 41.6667C672 33.3333 768 16.6667 864 16.6667C960 16.6667 1056 33.3333 1152 41.6667C1248 50 1344 50 1392 50L1440 50V100H1392C1344 100 1248 100 1152 100C1056 100 960 100 864 100C768 100 672 100 576 100C480 100 384 100 288 100C192 100 96 100 48 100H0V0Z" fill="white"/>
         </svg>
